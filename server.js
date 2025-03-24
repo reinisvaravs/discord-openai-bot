2d1ab15c9732b1d9c05052483f929571cea6bcdf
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-24 17:31:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,96 @@
+import dotenv from "dotenv";
+import { Client } from "discord.js";
+import { OpenAI } from "openai";
+dotenv.config();
+
+const client = new Client({
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
+});
+
+client.on("ready", () => {
+  console.log("The bot is online");
+});
+
+const IGNORE_PREFIX = "!";
+const CHANNELS = ["1353724696214110280"];
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(IGNORE_PREFIX)) return;
+  if (
+    !CHANNELS.includes(message.channelId) &&
+    !message.mentions.users.has(client.user.id)
+  )
+    return;
+
+  await message.channel.sendTyping();
+
+  const sendTypingInterval = setInterval(() => {
+    message.channel.sendTyping();
+  }, 5000);
+
+  let conversation = [];
+
+  conversation.push({
+    role: "system",
+    content: "Chat GPT is a friendly chatbot.",
+  });
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 10 });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (msg.author.bot && msg.author.id !== client.user.id) return;
+    if (msg.content.startsWith(IGNORE_PREFIX)) return;
+
+    const username = msg.author.username
+      .replace(/\s+/g, "_")
+      .replace(/[^\w\s]/gi, "");
+
+    if (msg.author.id === client.user.id) {
+      conversation.push({
+        role: "assistant",
+        name: username,
+        content: msg.content,
+      });
+      return;
+    }
+
+    conversation.push({
+      role: "user",
+      name: username,
+      content: msg.content,
+    });
+  });
+
+  const response = await openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: conversation,
+    })
+    .catch((error) => console.error("OpenAI Error:\n", error));
+
+  clearInterval(sendTypingInterval);
+
+  if (!response) {
+    message.reply(
+      "Im having some trouble with OpenAI API. Try again in a momemt."
+    );
+    return;
+  }
+
+  const responseMessage = response.choices[0].message.content;
+  const chunkSizeLimit = 2000;
+
+  for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+
+    await message.reply(chunk);
+  }
+});
+
+client.login(process.env.TOKEN);
